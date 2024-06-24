086817cd9f02030d339f5e11e25dd87bbbe027be
prani <pranithapushadapu7@gmail.com>
2024-06-24 20:57:55 +0530

    add redux perist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
 import { Link,useNavigate } from "react-router-dom"
 import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import {signInStart,signInFailure,signInSuccess} from '../redux/user/userSlice'
 
-export default function signIN() {
+
+export default function SignIn() {
   const [formData,setFormData]=useState({}) 
-  const [error,setError]=useState(null);
-  const [loading,setLoading]=useState(false);
+  const {loading, error}=useSelector((state)=>state.user);
   const navigate=useNavigate();
+  const dispatch=useDispatch();
   const handleChange = (e) => {
     setFormData(
       {
@@ -18,7 +21,7 @@ export default function signIN() {
   const handleSubmit = async (e)=>{
     e.preventDefault();
     try{
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',
         {
           method:'POST',
@@ -30,24 +33,21 @@ export default function signIN() {
       const data = await res.json();
      console.log(data);
       if(data.success === false){
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
-      navigate('/')
+      dispatch(signInSuccess(data));
+      navigate('/');
         
 
     }
     catch(error){
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
 
     }
    
   };
-  console.log(formData);
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className ='text-3xl text-center font-semibold my-9'>Sign In</h1>
